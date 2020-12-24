e9db97d8659157926bc5dc015933ce63ded0fb83
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2020-12-24 11:36:00 +0530

    1 file modified, 1 added

---
+++ src/environiments/environiments.ts
@@ -0,0 +1,11 @@
+export const environiment = {
+
+firebase : {
+  apiKey: "AIzaSyAOclQzxWig1ZBdgUipXlgmdV4paMr_jV4",
+  authDomain: "pepper-90233.firebaseapp.com",
+  projectId: "pepper-90233",
+  storageBucket: "pepper-90233.appspot.com",
+  messagingSenderId: "345794673518",
+  appId: "1:345794673518:web:4fe30588db052748d79f7f"
+}
+};

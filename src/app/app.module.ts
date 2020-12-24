4942a076adf3cea2ec514d863b16a2bfce7969f8
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2020-12-24 12:59:54 +0530

    create Service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CustomerService } from './customers/customer.service';
+import { CustomerDetailsComponent } from './customers/customer-details/customer-details.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, CustomerDetailsComponent, CustomersListComponent, CreateCustomerComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [CustomerService]
 })
 export class AppModule { }
